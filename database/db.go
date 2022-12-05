a0ab6d4e57a489c709ae1da7d830eb30ea82261e
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-05 12:50:15 -0300

    ambiente

---
+++ database/db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConnectDB() {
+	dsn := "root:root@tcp(127.0.0.1:3060)/gpuapi?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Panic("error connecting to database")
+	}
+}
