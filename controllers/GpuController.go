e996c6f0db1a8779ebd753b6107ed90824402ed7
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-05 13:27:19 -0300

    create new gpu

--- controllers/GpuController.go
+++ controllers/GpuController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"mymodule/database"
 	"mymodule/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,3 +13,20 @@ func AllGpus(c *gin.Context) {
 	database.DB.Find(&Gpu)
 	c.JSON(200, Gpu)
 }
+
+func CreateNewGpu(c *gin.Context) {
+	var Gpu models.Gpu
+	database.DB.Create(&Gpu)
+	c.JSON(http.StatusCreated, Gpu)
+}
+
+// if err := c.ShouldBindJSON(&Gpu); err != nil {
+// 	c.JSON(http.StatusBadRequest, gin.H{
+// 		"error": err.Error()})
+// 	return
+// }
+// if err := models.ValidateGpuData(&Gpu); err != nil {
+// 	c.JSON(http.StatusBadRequest, gin.H{
+// 		"error": err.Error()})
+// 	return
+// }
