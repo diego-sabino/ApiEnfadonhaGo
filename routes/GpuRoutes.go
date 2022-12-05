8b8497ee86f0359db42cdcca01cd7b9ead930b17
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-05 13:43:29 -0300

    route gpubyid

--- routes/GpuRoutes.go
+++ routes/GpuRoutes.go
@@ -10,6 +10,7 @@ func HandleRequests() {
 	r := gin.Default()
 	r.GET("/gpu", controllers.AllGpus)
 	r.POST("/gpu", controllers.CreateNewGpu)
+	r.GET("/gpu/:id", controllers.GetGpuById)
 	r.DELETE("/gpu/:id", controllers.DeleteGpu)
 	r.Run()
 }
