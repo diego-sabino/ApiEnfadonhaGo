2f8c6039b25b209549515ff36e40c93109ae0335
Diego-Sabino <dbrgamei5@gmail.com>
2022-12-05 12:58:17 -0300

    fix connecting db

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConnectDB() {
-	dsn := "root:root@tcp(127.0.0.1:3060)/gpuapi?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/gpuapi?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panic("error connecting to database")
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "mymodule/database"
 
+func main() {
+	database.ConnectDB()
 }
